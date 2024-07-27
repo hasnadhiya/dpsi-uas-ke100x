1e8c102da167b09082e70adce6d71a7ab8df4805
daci-noona <136562428+daci-noona@users.noreply.github.com>
2024-07-27 08:17:11 +0700

    last

--- config/database.js
+++ config/database.js
@@ -1,8 +1,15 @@
 const { Sequelize } = require('sequelize');
+const {mysql2} = require('mysql2');
 
-const sequelize = new Sequelize('dpsi-uas-ke100x', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql'
-});
+const sequelize = new Sequelize(
+  "sql12722347",
+  "sql12722347",
+  "tkynhsA4LV",
+  {
+    dialect: 'mysql',
+    dialectModule: mysql2,
+    host: "sql12.freesqldatabase.com",
+  }
+)
 
-module.exports = sequelize;
+module.exports = sequelize;
--- models/index.js
+++ models/index.js
@@ -1,19 +1,11 @@
-const { Sequelize } = require('sequelize');
+const sequelize = require('../config/database');
+const User = require('../models/user');
 
-const sequelize = new Sequelize('dpsi-uas-ke100x', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql'
-});
-
-console.log(sequelize)
-
- // Uji koneksi
- sequelize.authenticate()
- .then(() => {
- console.log('Connection has been established successfully.');
- })
- .catch(err => {
- console.error('Unable to connect to the database:', err);
- });
-
-module.exports = sequelize;
+// Sinkronisasi database
+sequelize.sync()
+    .then(() => {
+        console.log('Database & tables created!');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
